504d30bc3aaea2048e8282502c99ac77938ee159
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-05-07 22:03:21 +0200

    Base spring boot application

---
+++ src/main/java/pl/jp/XmlAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package pl.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlAnalyzerApplication.class, args);
+	}
+}
