3a5d6772c1c95b194ecfb6c9613d570cf704375e
Jakub Podeszwik <jakub.podeszwik@gino.nl>
2018-05-13 20:03:35 +0200

    importing service as an interface

--- src/main/java/pl/jp/analyzer/analysis/XmlPostFileAnalysisService.java
+++ src/main/java/pl/jp/analyzer/analysis/XmlPostFileAnalysisService.java
@@ -1,34 +1,9 @@
 package pl.jp.analyzer.analysis;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.util.stream.Stream;
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import org.springframework.stereotype.Service;
 
-@Service
-public class XmlPostFileAnalysisService {
-    private final XmlPostFileReader xmlPostFileReader;
-    private final PostStreamAnalyzer postStreamAnalyzer;
-
-    public XmlPostFileAnalysisService(XmlPostFileReader xmlPostFileReader, PostStreamAnalyzer postStreamAnalyzer) {
-        this.xmlPostFileReader = xmlPostFileReader;
-        this.postStreamAnalyzer = postStreamAnalyzer;
-    }
-
-    public PostStats analyze(URL url) throws IOException, XMLStreamException {
-        try (InputStream in = url.openStream()) {
-            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-            XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(in);
-            try {
-                Stream<Post> posts = xmlPostFileReader.readPosts(xmlEventReader);
-                return postStreamAnalyzer.analyzePosts(posts);
-            } finally {
-                xmlEventReader.close();
-            }
-        }
-    }
+public interface XmlPostFileAnalysisService {
+    PostStats analyze(URL url) throws IOException, XMLStreamException;
 }
---
+++ src/main/java/pl/jp/analyzer/analysis/XmlPostFileAnalysisServiceImpl.java
@@ -0,0 +1,34 @@
+package pl.jp.analyzer.analysis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.stream.Stream;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import org.springframework.stereotype.Service;
+
+@Service
+class XmlPostFileAnalysisServiceImpl implements XmlPostFileAnalysisService {
+    private final XmlPostFileReader xmlPostFileReader;
+    private final PostStreamAnalyzer postStreamAnalyzer;
+
+    public XmlPostFileAnalysisServiceImpl(XmlPostFileReader xmlPostFileReader, PostStreamAnalyzer postStreamAnalyzer) {
+        this.xmlPostFileReader = xmlPostFileReader;
+        this.postStreamAnalyzer = postStreamAnalyzer;
+    }
+
+    public PostStats analyze(URL url) throws IOException, XMLStreamException {
+        try (InputStream in = url.openStream()) {
+            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+            XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(in);
+            try {
+                Stream<Post> posts = xmlPostFileReader.readPosts(xmlEventReader);
+                return postStreamAnalyzer.analyzePosts(posts);
+            } finally {
+                xmlEventReader.close();
+            }
+        }
+    }
+}
